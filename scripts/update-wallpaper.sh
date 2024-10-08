da682c4985a1bee6ca9d872e21820b81f33ff836
flask <notflask@proton.me>
2024-10-08 19:34:03 +0200

    initial commit

---
+++ scripts/update-wallpaper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+ exit 1
+fi
+
+wallpaper_path="$1"
+dwm_path="$HOME/.suckless/dwm"
+wal_path="$HOME/.cache/wal"
+
+if [ ! -f "$wallpaper_path" ]; then
+ exit 1
+fi
+
+wal -i "$wallpaper_path"
+cp "$wal_path/colors-wal-dwm.h" "$dwm_path/wal.h"
+cd "$dwm_path"
+sudo make clean install
---
+++ suckless.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+installation_dir="$(dirname "$(realpath "$0")")"
+suckless_cfg_dir="$HOME/.suckless"
+suckless_apps=("st" "dwm" "scroll")
+
+if ! [ -d $suckless_cfg_dir ] ; then
+	# clone suckless's applications 
+	git clone git@gitlab.com:n0tflask/suckless.git "$suckless_cfg_dir"
+
+	# build it all 
+	for app in "${suckless_apps[@]}"; do
+		cd "$suckless_cfg_dir/$app"
+		sudo make clean install
+	done
+
+	# copy .xinitrc so startx will work w/ dwm
+	cp "$installation_dir/.xinitrc" "$HOME/.xinitrc"
+else
+	echo "suckless's apps already installed"
+fi
+	
+
