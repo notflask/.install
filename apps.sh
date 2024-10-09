73f6b5bfb52523f5379f013615725c8850190e9b
Daniel «flask» <notflask@proton.me>
2024-10-09 23:47:42 +0200

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 mv scripts/*.sh ~/.bin
+
+if ! [ -v yay &> /dev/null ]; then
+  git clone https://aur.archlinux.org/yay
+  cd yay
+  makepkg -si
+fi 
+
+yay -S dmenu firefox telegram-desktop-bin vesktop-bin gcc clang neovim
